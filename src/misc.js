3b5e885fbcf3fc6c5532b57ea99c3bad5f5f142b
submission <50590820+conf-submission-anon@users.noreply.github.com>
2019-11-01 21:58:39 -0400

    Submission

---
+++ src/misc.js
@@ -0,0 +1,30 @@
+function base64DecodeToBlob(b64Data, contentType, sliceSize) {
+  contentType = contentType || '';
+  sliceSize = sliceSize || 512;
+
+  var byteCharacters = atob(b64Data);
+  var byteArrays = [];
+
+  for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+    var slice = byteCharacters.slice(offset, offset + sliceSize);
+
+    var byteNumbers = new Array(slice.length);
+    for (var i = 0; i < slice.length; i++) {
+      byteNumbers[i] = slice.charCodeAt(i);
+    }
+
+    var byteArray = new Uint8Array(byteNumbers);
+
+    byteArrays.push(byteArray);
+  }
+
+  var blob = new Blob(byteArrays);
+  return blob;
+}
+
+
+function base64sha256(b64Data){
+  var Base64Sha256 = new jsSHA("SHA-256", "B64");
+  Base64Sha256.update(b64Data);
+  return Base64Sha256.getHash("HEX");
+}
